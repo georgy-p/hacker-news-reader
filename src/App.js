5e0b7a7f482bd7e9af7616258b0cc85799abd93e
George <81578359+georgy-p@users.noreply.github.com>
2023-03-01 19:46:45 +0300

    add Routes

--- src/App.js
+++ src/App.js
@@ -1,43 +1,17 @@
-import { Button, Layout, Space, Typography, theme } from 'antd';
-import 'antd/dist/reset.css';
+import React from 'react';
+import { Routes, Route, } from 'react-router-dom';
+import BaseLayout from './Components/Routes/BaseLayout.jsx';
 import './App.css';
+import MainPage from './Components/MainPage.jsx';
 
-const { Header, Footer, Content } = Layout;
-const { Title } = Typography;
-const { colorBgContainer } = theme;
-
-const headerStyle = {
-  textAlign: 'left',
-  color: '#fff',
-  height: 64,
-  paddingInline: 40,
-  lineHeight: '64px',
-  backgroundColor: '#FF6C00',
-};
-
-const brandTitleStyle = {
-  textAlign: 'left',
-  paddingTop: '1em',
-};
 
 
 const App = () => (
-  <Space direction="vertical" style={{ width: '100%' }} size={[0, 48]}>
-    <Layout className="layout">
-    <Header style={headerStyle}>
-      <Title level={3} style={brandTitleStyle}>
-        The Best Hacker News Reader
-      </Title>
-      </Header>
-      <Content style={{ padding: '0 50px' }}>
-      <div className="site-layout-content" style={{ background: colorBgContainer }}>
-          Content
-          <Button type="primary">Button</Button>
-        </div>
-    </Content>
-  <Footer>Georgy Pigar</Footer>
-  </Layout>
-  </Space>
+  <Routes>
+    <Route element={<BaseLayout />}>
+      <Route path="/" element={<MainPage />} />
+    </Route>
+  </Routes>
 )
 
 export default App;
